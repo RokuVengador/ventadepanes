e35ed783fe1d66f2e615b95b4f2d85805e2fbe9b
RokuVengador <125324301+RokuVengador@users.noreply.github.com>
2023-03-29 23:18:58 -0600

    Add files via upload
    el fin es aprender a usar el parse

---
+++ ventadepanes/ventadepanes/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Ingrese precio de fresco");
+string numfresco = Console.ReadLine();
+
+Console.WriteLine("Ingrese cantidad de fresco");
+string cantfresco = Console.ReadLine();
+
+
+Console.WriteLine("Ingrese precio de panes");
+string numpanes = Console.ReadLine();
+
+Console.WriteLine("Ingrese cantidad de panes");
+string cantpanes = Console.ReadLine();
+
+
+float numfrescof = float.Parse(numfresco);
+float cantfrescof = float.Parse(cantfresco);
+
+float numpanesf = float.Parse(numpanes);
+float cantpanesf = float.Parse(cantpanes);
+
+
+float resultadofresco = numfrescof * cantfrescof;
+float resultadopanes = numpanesf * cantpanesf;
+
+float total = resultadofresco + resultadopanes;
+Console.WriteLine("Total a pagar");
+
+Console.WriteLine("De fresco: "+resultadofresco);
+Console.WriteLine("De panes: "+resultadopanes);
+Console.WriteLine(total);
